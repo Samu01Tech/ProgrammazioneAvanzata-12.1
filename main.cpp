21cce7429a7fa9b29058205144c6ea024cf04c03
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-12-15 14:45:45 +0000

    basics

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+//al posoto di fare questo
+//int massimo(int x, int y);
+//float massimo(float x, float y);
+
+template <typename T> T massimo(T x, T y){
+    if(x < y) return y; 
+    else return x;
+}
+
 int main() {
-  cout << "Hello World!\n";
+    int a = 2, b = 5;
+    cout <<  massimo(a, b) << endl;
+
+    float c = 3.14, d = 0.02;
+    cout << massimo(c, d) << endl;
 } 
